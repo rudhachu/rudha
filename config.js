f4c1993b2617c46e01ea1f19eb2723166315f92c
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-01 21:41:37 +0530

    Initial commit

---
+++ config.js
@@ -0,0 +1,63 @@
+const isVPS = !(__dirname.startsWith("/rudhra-bot") || __dirname.startsWith("/skl"));
+const isHeroku = __dirname.startsWith("/rudhra-bot");
+const isKoyeb = __dirname.startsWith("/rudhra-bot");
+const isRailway = __dirname.startsWith("/rudhra-bot");
+const { Sequelize } = require("sequelize");
+const fs = require("fs");
+//require("dotenv").config();
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+const toBool = (x) => x === "true";
+
+const DATABASE_URL = process.env.DATABASE_URL || "./assets/database.db";
+const ACR_A = process.env.ACR_A || "ff489a0160188cf5f0750eaf486eee74";
+const ACR_S = process.env.ACR_A || "ytu3AdkCu7fkRVuENhXxs9jsOW4YJtDXimAWMpJp";
+global.website = "https://whatsapp.com/channel/0029Vag5l2ALSmbi14YryJ2r"
+module.exports = {
+  ANTILINK: process.env.ANTI_LINK || "on", //on or off
+  LOGS: toBool(process.env.LOGS) || true,
+  ANTI_LINK_ACTION: process.env.ANTI_LINK_ACTION || "delete", //delete or kick
+  MENTION: process.env.MENTION || "enable",
+  SESSION_ID: process.env.SESSION_ID || "taira_tech-X40HgARY#J9iLi68W66RyC5wrkh9fEABegbS1migGoy4UD92Vft4",//Session ID here
+  LANG: process.env.LANG || "EN",//Only English currently supported
+  HANDLERS: process.env.HANDLER === "false" || process.env.HANDLER === "null" ? "^" : "^[/]",
+  RMBG_KEY: process.env.RMBG_KEY || false,
+  PLATFORM:isHeroku?"Heroku":isRailway?"Railway":isKoyeb?"Koyeb":"Other server",isHeroku,isKoyeb,isVPS,isRailway,
+  BRANCH: "main",
+  WARN_COUNT: 3,
+  PACKNAME: process.env.PACKNAME || "ʀᴜᴅʜʀᴀ-ʙᴏᴛ",
+  PRESENCE: process.env.PRESENCE || "recording",
+  WELCOME_MSG: process.env.WELCOME_MSG || `Hi @user Welcome to @gname\n\nʀᴜᴅʜʀᴀ-ʙᴏᴛ`,
+  GOODBYE_MSG: process.env.GOODBYE_MSG || `Hi @user It was Nice Seeing you\n\nʀᴜᴅʜʀᴀ-ʙᴏᴛ`,
+  AUTHOR: process.env.AUTHOR || "ʀᴜᴅʜʀᴀ-ʙᴏᴛ",
+  SUDO: process.env.SUDO || "919895809960",//Also sudo numbers
+  HEROKU_APP_NAME: process.env.HEROKU_APP_NAME || "",
+  HEROKU_API_KEY: process.env.HEROKU_API_KEY || "",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "919895809960",
+  OWNER_NAME: process.env.OWNER_NAME || "ʀᴜᴅʜʀᴀ-ʙᴏᴛ",
+  HEROKU: toBool(process.env.HEROKU) || true,
+  BOT_NAME: process.env.BOT_NAME || "ʀᴜᴅʜʀᴀ-ʙᴏᴛ",
+  PROCESSNAME: process.env.PROCESSNAME || "ʀᴜᴅʜʀᴀ-ʙᴏᴛ",
+  WORK_TYPE: process.env.WORK_TYPE || "private",
+  ANTIDELETE: toBool(process.env.DELETED_LOG) || false,
+  REMOVEBG : process.env.REMOVEBG || false,
+  DATABASE_URL: DATABASE_URL,
+  STATUS_SAVER: toBool(process.env.STATUS_SAVER) || true, //make true to auto send status
+  DATABASE:
+    DATABASE_URL === "./database.db"
+      ? new Sequelize({
+          dialect: "sqlite",
+          storage: DATABASE_URL,
+          logging: false,
+        })
+      : new Sequelize(DATABASE_URL, {
+          dialect: "postgres",
+          ssl: true,
+          protocol: "postgres",
+          dialectOptions: {
+            native: true,
+            ssl: { require: true, rejectUnauthorized: false },
+          },
+          logging: false,
+        }),
+};
