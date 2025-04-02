0b7fef66f8bb80535f8ca731f90765329bd60f6c
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-02 21:14:25 +0530

    Update sticker.js

--- plugins/sticker.js
+++ plugins/sticker.js
@@ -1,21 +1,8 @@
-/*
-██╗  ██╗ ██████╗ ████████╗ █████╗ ██████╗  ██████╗       ███╗   ███╗██████╗ 
-██║  ██║██╔═══██╗╚══██╔══╝██╔══██╗██╔══██╗██╔═══██╗      ████╗ ████║██╔══██╗
-███████║██║   ██║   ██║   ███████║██████╔╝██║   ██║█████╗██╔████╔██║██║  ██║
-██╔══██║██║   ██║   ██║   ██╔══██║██╔══██╗██║   ██║╚════╝██║╚██╔╝██║██║  ██║
-██║  ██║╚██████╔╝   ██║   ██║  ██║██║  ██║╚██████╔╝      ██║ ╚═╝ ██║██████╔╝
-╚═╝  ╚═╝ ╚═════╝    ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝       ╚═╝     ╚═╝╚═════╝
- By : Taira Makino
- Github : https://github.com/anonphoenix007
- WhatsApp : https://wa.me/2347080968564
-*/                                                                                                                                                    
-
-
 const config = require("../config");
-const { command, isPrivate, toAudio } = require("../lib/");
+const { command, isPrivate, toAudio, addExif, AddMp3Meta, getBuffer } = require("../lib/");
 const { webp2mp4, textToImg } = require("../lib/functions");
-command(
-  {
+
+command({
     pattern: "sticker",
     fromMe: isPrivate,
     desc: "Converts Photo/video/text to sticker",
@@ -42,31 +29,73 @@ command(
     message.sendMessage(
       message.jid,
       buff,
-      { packname: '☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬', author: config.AUTHOR },
+      { packname: config.STICKER_DATA.split(";")[0], author: config.STICKER_DATA.split(";")[1], },
       "sticker"
     );
   }
 );
 
-command(
-  {
-    pattern: "take",
-    fromMe: isPrivate,
-    desc: "Converts Photo or video to sticker",
-    type: "converter",
-  },
-  async (message, match, m) => {
-    if (!message.reply_message.sticker)
-      return await message.reply("_Reply to a sticker_");
-    const packname = match.split(";")[0] || '☬ ʜᴏᴛᴀʀᴏ-ᴍᴅ ☬';
-    const author = match.split(";")[1] || config.AUTHOR;
-    let buff = await m.quoted.download();
-    message.sendMessage(message.jid, buff, { packname, author }, "sticker");
+command({
+  pattern: "take ?(.*)",
+  fromMe: isPrivate,
+  desc: "Change audio title, album/sticker author, packname",
+  type: "converter",
+}, async (message, match) => {
+  if (!message.reply_message || !(message.reply_message.video || message.reply_message.audio || message.reply_message.sticker)) {
+    return await message.reply("*_Reply to sticker/audio/voice/video!_*");
   }
-);
 
-command(
-  {
+  if (message.reply_message.audio || message.reply_message.video) {
+   let media = await toAudio(await message.quoted.download("buffer"));
+    
+    let matchParts = match ? match.match(/[^,;]+/g) : [];
+    let configParts = config.AUDIO_DATA.match(/[^,;]+/g);
+    
+    let url = matchParts[2] ? matchParts[2] : (configParts[2] ? configParts[2] : '');
+    let cover = await getBuffer(url); 
+
+    let title = matchParts[0] ? matchParts[0] : (configParts[0] ? configParts[0] : config.AUDIO_DATA);
+    let artist = matchParts[1] ? matchParts[1] : (configParts[1] ? configParts[1] : config.AUDIO_DATA);
+    artist = [artist]; // Ensure artist is an array
+
+    const res = await AddMp3Meta(media, cover, {
+      title: title,
+      artist: artist
+    });
+
+    return await message.client.sendMessage(message.jid, {
+      audio: res,
+      mimetype: "audio/mpeg"
+    }, {
+      quoted: message.data
+    });
+  } else if (message.reply_message.sticker) {
+    let q = await message.reply_message.download("buffer");
+    let exif;
+    if (match !== "") {
+      const splitMatch = match.split(/[,;]/);
+      exif = {
+        author: splitMatch[1] ? splitMatch[1] : "",
+        packname: splitMatch[0] ? splitMatch[0] : "",
+        categories: config.STICKER_DATA.split(";")[2] || "",
+        android: "https://github.com/princerudh/",
+        ios: "https://github.com/princerudh/"
+      };
+    } else {
+      exif = {
+        author: config.STICKER_DATA.split(/[,;]/)[1] || "",
+        packname: config.STICKER_DATA.split(/[,;]/)[0] || "",
+        categories: config.STICKER_DATA.split(";")[2] || "",
+        android: "https://github.com/princerudh/",
+        ios: "https://github.com/princerudh/"
+      };
+    }
+    let stickerBuffer = await addExif(q, exif);
+    return await message.client.sendMessage(message.jid, { sticker: fs.readFileSync(stickerBuffer) }, { quoted: message.data });
+  }
+});
+
+command({
     pattern: "photo",
     fromMe: isPrivate,
     desc: "Changes sticker to Photo",
@@ -80,29 +109,43 @@ command(
   }
 );
 
-command(
-  {
+command({
     pattern: "mp3",
     fromMe: isPrivate,
     desc: "converts video/voice to mp3",
     type: "media",
-  },
-  async (message, match, m) => {
-    let buff = await m.quoted.download();
-    console.log(typeof buff);
-    buff = await toAudio(buff, "mp3");
-    console.log(typeof buff);
-    return await message.sendMessage(
-      message.jid,
-      buff,
-      { mimetype: "audio/mpeg" },
-      "audio"
-    );
-  }
-);
+}, async (message, match) => {
+  try {
+   let media = await toAudio(await message.quoted.download("buffer"));
+    
+    let matchParts = match ? match.match(/[^,;]+/g) : [];
+    let configParts = config.AUDIO_DATA.match(/[^,;]+/g);
+    
+    let url = matchParts[2] ? matchParts[2] : (configParts[2] ? configParts[2] : '');
+    let cover = await getBuffer(url); 
 
-command(
-  {
+    let title = matchParts[0] ? matchParts[0] : (configParts[0] ? configParts[0] : config.AUDIO_DATA);
+    let artist = matchParts[1] ? matchParts[1] : (configParts[1] ? configParts[1] : config.AUDIO_DATA);
+    artist = [artist]; // Ensure artist is an array
+
+    const res = await AddMp3Meta(media, cover, {
+      title: title,
+      artist: artist
+    });
+
+    return await message.client.sendMessage(message.jid, {
+      audio: res,
+      mimetype: "audio/mpeg"
+    }, {
+      quoted: message.data
+    });
+  } catch (error) {
+    console.error("Error:", error);
+    return await message.send("An error occurred while processing your request.");
+  }
+});
+
+command({
     pattern: "mp4",
     fromMe: isPrivate,
     desc: "converts video/voice to mp4",
@@ -130,8 +173,7 @@ command(
   }
 );
 
-command(
-  {
+command({
     pattern: "img",
     fromMe: isPrivate,
     desc: "Converts Sticker to image",
