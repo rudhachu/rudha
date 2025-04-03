39bac073a1cba7cf49169dd2e999230c1fd2e2dd
rudhachu <120007671+rudhachu@users.noreply.github.com>
2025-04-03 11:13:55 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -5,11 +5,10 @@ command({
     fromMe: isPrivate,
     desc: "check bot speed",
     type: "user",
-  },
-    async (message, match, m) => {
+  },async message => {
     const start = new Date().getTime();
     const msg = await message.reply('Checking!');
     const end = new Date().getTime();
     const responseTime = (end - start) / 1000;
-    return await msg.edit(`*Response: ${responseTime} secs*`);
+    await msg.edit(`*Response: ${responseTime} secs*`);
 });
